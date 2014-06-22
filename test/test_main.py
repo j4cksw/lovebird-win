44e865fadf516178fc2d7f72acae84eb84f25643
j4cksw <ninjaadvance@gmail.com>
2014-06-22 19:18:15 +0700

    Add Main class

---
+++ test/test_main.py
@@ -0,0 +1,30 @@
+import unittest
+import wx
+from mock import Mock
+from Main import Main
+
+class MainTest(unittest.TestCase):
+    
+    
+    def setUp(self):
+        self.app = wx.App()
+        self.app.MainLoop = Mock()
+        
+        self.frame = wx.Frame(None)
+        self.frame.Show = Mock()
+        
+        self.main = Main()
+        self.main.app = self.app
+        self.main.frame = self.frame
+        
+        
+    def test_start_application_main_loop(self):
+        self.main.open()
+        
+        self.app.MainLoop.assert_called_with()
+
+    
+    def test_show_application_main_frame(self):
+        self.main.open()
+        
+        self.frame.Show.assert_called_with()
